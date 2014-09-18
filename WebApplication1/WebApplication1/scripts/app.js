7c2c6c6811930133ccd1580b01c90dab7e49fc90
Tony-Mc <maciulevicius.tony@gmail.com>
2014-09-18 12:43:34 +0300

    First

---
+++ WebApplication1/WebApplication1/scripts/app.js
@@ -0,0 +1,21 @@
+﻿requirejs.config({
+    //By default load any module IDs from scripts/lib
+    baseUrl: 'scripts/lib',
+    paths: {
+        models: '../models',
+        collections: '../collections',
+        views: '../views',
+        routers: '../routers',
+        components: '../components',
+        modalDialog: 'backbone.ModalDialog'
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
+    }
+});
