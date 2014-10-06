52daa25a6cea190ceee6caee631cd2230f0ccdfa
ignassav <ignas.savanevicius@gmail.com>
2014-10-06 17:36:23 +0300

    Sortable

--- UI/UI/scripts/views/metricListView.js
+++ UI/UI/scripts/views/metricListView.js
@@ -18,12 +18,27 @@
         },
 
         initialize: function () {
-            this.collection = new MetricCollection();
+            if (this.collection) {
+                this.collection.on('add', this.render, this);
+                this.collection.on('remove', this.render, this);
+                this.collection.on('fetch', this.render, this);
+            } else {
+                this.collection = new MetricCollection();
+                this.collection.on('remove', this.render, this);
+                this.collection.on('fetch', this.render, this);
+            }
             this.collection.on('add', this.render, this);
         },
 
         render: function () {
             this.$el.html(this.template({ "Metrics": this.collection.toJSON() }));
+            $('.sortable').sortable({
+                items: 'li',
+                placeholder: '<tr><td colspan="7">&nbsp;</td></tr>'
+            }).bind('sortupdate', function (e, ui) {
+
+            });
+
         },
 
         addMetric: function () {
