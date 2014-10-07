f3ad9c371d59cfb5182aa22e5b8ec5fe57989548
Mikalojus Galminas <mikalojus.galminas@gmail.com>
2014-10-07 11:29:50 +0300

    Installer

--- API/Host/Program.cs
+++ API/Host/Program.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.Owin.Hosting;
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Mime;
+using System.Web.Configuration;
+using Microsoft.Owin.Hosting;
 using System;
 using System.ServiceProcess;
+using System.Configuration;
 
 namespace ApiHost
 {
@@ -34,6 +40,36 @@ namespace ApiHost
 
         private static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                if (args[0].Equals("-install"))
+                {
+                    var loc = System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase;
+                    var location = loc.Remove(0, 8); //file:///c:/...
+
+                    var process = new Process();
+                    ProcessStartInfo info = new ProcessStartInfo();
+                    info.Verb = "runas";
+                    info.FileName = "cmd.exe";
+                    info.RedirectStandardInput = true;
+                    info.UseShellExecute = false;
+
+                    process.StartInfo = info;
+                    process.Start();
+                    using (StreamWriter sw = process.StandardInput)
+                    {
+                        if (sw.BaseStream.CanWrite)
+                        {
+                            sw.WriteLine("sc stop \"Report Builder\"");
+                            sw.WriteLine("sc delete \"Report Builder\"");
+                            sw.WriteLine("sc create \"Report Builder\" binpath= \"" + location + "\"");
+                            sw.WriteLine("sc start \"Report Builder\"");
+                            sw.WriteLine("cls");
+                        }
+                    }
+                }
+            }
+            
             //Launches as console application when Environment.UserInteractive if true. Otherwise it launches as a service.
             if (!Environment.UserInteractive)
             {
@@ -42,8 +78,7 @@ namespace ApiHost
             }
             else
             {
-                Console.WriteLine("Running as console application");
-                Console.WriteLine("Press Enter to Quit . . .");
+                Console.WriteLine("Running");
                 Start(args);
                 Console.ReadKey();
                 Stop();
@@ -52,12 +87,10 @@ namespace ApiHost
         }
 
         //TODO: http://stackoverflow.com/questions/2456819/how-can-i-set-up-net-unhandledexception-handling-in-a-windows-service
-        private static void Start(string[] args) 
+        private static void Start(string[] args)
         {
-            //TODO move to config
-            const string baseUrl = "http://localhost:5000/";
-
-            _webHost = WebApp.Start<Startup>(baseUrl);
+            var ip = ConfigurationManager.AppSettings["ip"];
+            _webHost = WebApp.Start<Startup>(ip);
         }
 
         private static void Stop()
