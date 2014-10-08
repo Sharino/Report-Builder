c991530729b33a0200e7a6d020807ca09c5069bc
Tony-Mc <maciulevicius.tony@gmail.com>
2014-10-08 19:44:57 +0300

    UI: Sortable Order
    Changes:
    - metricListView.js:
    * Updated .sortable ordering. Items will be shifted accordingly every
    time user switch some elements.
    * DEBUG:
    ~Added sorting on render.
    ~Added re-render after switching.

--- UI/UI/scripts/views/metricListView.js
+++ UI/UI/scripts/views/metricListView.js
@@ -14,7 +14,7 @@
         template: _.template(MetricListTemplate),
 
         events: {
-            'click #addMetric': 'addMetric',
+            'click #addMetric': 'addMetric'
         },
 
         initialize: function () {
@@ -31,6 +31,7 @@
         },
 
         render: function () {
+            this.collection.sort();                                                // DEBUG: Sorting might not be required later as we implement features.
             this.$el.html(this.template({ "Metrics": this.collection.toJSON() })); // Render Metric list
 
             var self = this;
@@ -41,13 +42,23 @@
                 forcePlaceholderSize: true,
                 placeholder: '<li>Placeholder</li>'
             }).bind('sortupdate', function (e, ui) {
-                /* Get collection items with changed Order. */
-                var firstItem = self.collection.findWhere({ Order: ui.oldindex });
-                var secondItem = self.collection.findWhere({ Order: ui.item.index() });
+                var draggedItem = self.collection.findWhere({ Order: ui.oldindex }); // The item user dragged
 
-                /* Set new Order to selected items. */
-                firstItem.set({ Order: ui.item.index() });
-                secondItem.set({ Order: ui.oldindex });
+                if (ui.oldindex > ui.item.index()) {                                 // User dragged left
+                    for (var i = ui.item.index(); i < ui.oldindex; i++) {            // For every item in between new id and old id we increase Order, because it shifted right by 1
+                        var item = self.collection.at(i);                            // Get item at array position [i]. Position is NOT Order, it is just insert position.
+                        item.set({ Order: i + 1 });                                  // Increase order by 1
+                    }
+                    draggedItem.set({ Order: ui.item.index() });                     // Set new Order to dragged item.
+                }
+                else {                                                               // User dragged right
+                    for (var i = ui.oldindex + 1; i <= ui.item.index() ; i++) {      // Same idea all over, just decrease the order cuz items shifted left.
+                        var item = self.collection.at(i);
+                        item.set({ Order: i - 1 });
+                    }
+                    draggedItem.set({ Order: ui.item.index() });
+                }
+                self.render();                                                       // DEBUG: Rerender list. Currently we show order in braces, ex.: {Metric title}{Position} ({Order})
             });
         },
 
