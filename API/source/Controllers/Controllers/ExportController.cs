9c46e9916c6b68bae80dc7270414103541e096d0
Mikalojus Galminas <mikalojus.galminas@gmail.com>
2014-11-11 16:23:02 +0200

    back-end csv export

--- API/source/Controllers/Controllers/ExportController.cs
+++ API/source/Controllers/Controllers/ExportController.cs
@@ -5,81 +5,111 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Controllers.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ExportController : ApiController
     {
-        //[HttpGet]
-        //public HttpResponseMessage GetFile(string id)
-        //{
-        //    string fileName = @"2014-11-10 02;23-CSV.csv";
-        //    string localFilePath = @"C:\Report Builder\2014-11-10 02;23-CSV.csv";
-        //    int fileSize;
-
-        //  //  localFilePath = getFileFromID(id, out fileName, out fileSize);
-
-        //    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
-        //    response.Content = new StreamContent(new FileStream(localFilePath, FileMode.Open, FileAccess.Read));
-        //    response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
-        //    response.Content.Headers.ContentDisposition.FileName = fileName;
-
-        //    return response;
-        //}
-
         [HttpPost]
-        public HttpResponseMessage ExportToCsv(List<Request> requests, string separator = ", ")
+        public HttpResponseMessage KpiToCsv(List<Values> request, string separator = ",")
         {
-            string header = "Date";
-            var headerList = new List<string>();
-            string content = "";
-
-            foreach (var req in requests)
-            {
-                content += req.Date;
-                foreach (var val in req.Values)
+            if (request != null)
+                if (request.Count > 0)
                 {
-                    if (!headerList.Contains(val.Mnemonic))
-                        headerList.Add(val.Mnemonic);
-                    content += separator + val.Value;
+                    string header = "";
+                    string content = "";
+
+                    Random random = new Random();
+                    int randomNumber = random.Next(100, 10000);
+
+                    string fileName = DateTime.UtcNow.ToString("yyyy-M-d dd;mm") + " - " + randomNumber + ".csv";
+                    string filePath = @"C:\Report Builder\" + fileName;
+
+                    foreach (var val in request)
+                    {
+                        header += val.Key + separator;
+                        content += val.Value + separator;
+                    }
+                    header = header.TrimEnd(separator.ToCharArray());
+                    content = content.TrimEnd(separator.ToCharArray());
+
+                    using (var fs = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+                    using (var sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine(header);
+                        sw.Write(content);
+                        sw.Close();
+                        fs.Close();
+                    }
+                    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                    response.Content = new StreamContent(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+                    response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+                    response.Content.Headers.ContentDisposition.FileName = fileName;
+                    return response;
                 }
-                content += "\n";
-            }
-            header = headerList.Aggregate(header, (current, mnemonic) => current + (separator + mnemonic));
-
-            Random random = new Random();
-            int randomNumber = random.Next(100, 7777);
-
-            string fileName = DateTime.UtcNow.ToString("yyyy-M-d") + " - " + randomNumber + "-CSV.csv";
-            string filePath = @"C:\Report Builder\" + fileName;
-
-
-            using (var fs = new FileStream(filePath, FileMode.Append, FileAccess.Write))
-            using (var sw = new StreamWriter(fs))
-            {
-                sw.WriteLine(header);
-                sw.Write(content);
-                sw.Close();
-                fs.Close();
-            }
-            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new StreamContent(new FileStream(filePath, FileMode.Open, FileAccess.Read));
-            response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
-            response.Content.Headers.ContentDisposition.FileName = fileName;
-
-            return response;
+            return Request.CreateResponse(HttpStatusCode.BadRequest);
         }
+
+        //[HttpPost]
+        //public HttpResponseMessage ExportToCsv(List<Request> requests, string separator = ", ")
+        //{
+        //    if (requests != null)
+        //        if (requests.Count > 0)
+        //        {
+        //            string header = "Date";
+        //            var headerList = new List<string>();
+        //            string content = "";
+
+        //            foreach (var req in requests)
+        //            {
+        //                content += req.Date;
+        //                foreach (var val in req.ComponentValues)
+        //                {
+        //                    if (!headerList.Contains(val.Key))
+        //                        headerList.Add(val.Key);
+        //                    content += separator + val.Value;
+        //                }
+        //                content += "\n";
+        //            }
+        //            header = headerList.Aggregate(header, (current, mnemonic) => current + (separator + mnemonic));
+
+        //            Random random = new Random();
+        //            int randomNumber = random.Next(100, 10000);
+
+        //            string fileName = DateTime.UtcNow.ToString("yyyy-M-d dd;mm") + " - " + randomNumber + ".csv";
+        //            string filePath = @"C:\Report Builder\" + fileName;
+
+
+        //            using (var fs = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+        //            using (var sw = new StreamWriter(fs))
+        //            {
+        //                sw.WriteLine(header);
+        //                sw.Write(content);
+        //                sw.Close();
+        //                fs.Close();
+        //            }
+        //            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+        //            response.Content = new StreamContent(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+        //            response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+        //            response.Content.Headers.ContentDisposition.FileName = fileName;
+
+        //            return response;
+        //        }
+        //    return Request.CreateResponse(HttpStatusCode.BadRequest);
+        //}
     }
 
     public class Values
     {
-        public string Mnemonic { get; set; }
+        public string Key { get; set; }
         public string Value { get; set; }
     }
 
     public class Request
     {
-        public List<Values> Values { get; set; }
+        public List<Values> ComponentValues { get; set; }
         public string Date { get; set; }
     }
 }
