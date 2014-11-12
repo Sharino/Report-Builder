7efa7ea92b901a079a6eb728eef7b85a84093ff1
Sharino <saruna.misiunas@gmail.com>
2014-11-12 18:26:56 +0200

    as

--- BuildAndDeployment/Deploy.ps1
+++ BuildAndDeployment/Deploy.ps1
@@ -45,8 +45,8 @@ foreach ($server in $servers)
     Write-Host `n:: Deploying to server $serverIP`n   
 
     Write-Host `n:: [REMOTE] Stopping service`n
-	Write-Host `n:: "sc stop "$serviceName""
-    $stopCommand = "sc stop "$serviceName""
+	Write-Host `n:: 'sc stop "$serviceName"'
+    $stopCommand = 'sc stop "$serviceName"'
     Start-And-Wait-For-Remote-Process $serverIP $stopCommand
 	
 	Write-Host `n:: [REMOTE] Deleting service`n
