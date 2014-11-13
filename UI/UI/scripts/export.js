808d89882c2a7172ed5d3dd050a5d021cc3389f3
Sharino <saruna.misiunas@gmail.com>
2014-11-13 17:04:19 +0200

    asda

--- UI/UI/scripts/export.js
+++ UI/UI/scripts/export.js
@@ -8,7 +8,7 @@
 
         exportCsv: function (data) {
             $.ajax({
-                url: 'http://37.157.0.42/:33895/api/Export',
+                url: 'http://37.157.0.42:33895/api/Export',
                 data: JSON.stringify(data),
                 contentType: 'application/json',
                 type: 'POST',
