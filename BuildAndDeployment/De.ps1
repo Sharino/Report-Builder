eade31880d69690a264e712bcee7c9299184d9e4
Sharino <saruna.misiunas@gmail.com>
2014-11-18 18:34:08 +0200

    ASD

--- BuildAndDeployment/De.ps1
+++ BuildAndDeployment/De.ps1
@@ -54,7 +54,7 @@ foreach ($server in $servers)
     Remove-Item -path $serviceFilesShare -Force
 
     Write-Host `n:: Copying new files`n
-    $source = "..\API\source\Host\bin\*"
+    $source = "..\API\source\Host\bin\Release\*"
     $destination = ($serviceFilesLocation -f $serverDomain)
     Copy-Item -Path $source -Destination $destination -Recurse -Force -Verbose -ErrorAction Stop
     Write-Host "Done"
