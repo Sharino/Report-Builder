db177610a0edb1737fe8f24931fc0f2f639d5333
Sharino <saruna.misiunas@gmail.com>
2014-11-20 20:34:39 +0200

    lonely am I so alonlyses

--- UI/UI/scripts/config/config.js
+++ UI/UI/scripts/config/config.js
@@ -1,26 +1,37 @@
 ﻿define('Config', [
 ], function () {
-    var baseUrl = "http://172.22.22.33:33894/api/";
+    //TODO using configą, pagal mode žiūrėt
+    var baseUrl = "http://37.157.0.42:33895/api/";
+    var baseUrl = "http://localhost:33894/api/";
     var einsteinUrl = "http://37.157.0.42:33896/api/";
 
     var Config = {
         ComponentSettings: {
             URL: baseUrl + "ReportComponent"
         },
+
         DashboardSettings: {
             URL: baseUrl + "Dashboard"
         },
+
         DashboardComponentSettings: {
             URL: baseUrl + "DashboardComponent"
         },
+
         MetricSettings: {
             URL: baseUrl + "Metric"
         },
+
         EinsteinSettings: {
             URL: einsteinUrl + "Einstein"
         },
+
         DimensionSettings: {
             URL: baseUrl + "Dimension"
+        },
+
+        MetricDimensionMap: {
+            URL: baseUrl + "MetricDimensionMap",
         },
 
         ExportSettings: {
@@ -28,6 +39,7 @@
             KpiToPDF: baseUrl + "Export/KpiToPdf"
         },
 
+       
         NotificationSettings: {
             Timeout: 1500
         },
@@ -53,7 +65,27 @@
             ErrorMessages: {
                 NoResponse: "Server did not respond."
             }
-        }
+        },
+
+        getMap: function () {
+            var self = this;
+            $.ajax({
+                url: "http://localhost:33894/api/MetricDimensionMap",
+                contentType: 'application/json',
+                type: 'GET',
+                success: function (response) {
+                    self.map = response;
+                },
+                error: function () {
+                    $.notifications.display({
+                        type: 'error',
+                        content: "Critical error. PRESS ALT + F4",
+                        timeout: Config.NotificationSettings.Timeout
+                    });
+                }
+            });
+        },
+
     };
 
     return Config;
