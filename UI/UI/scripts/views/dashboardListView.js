cb0acfd4657cecfc93557cfa4534890b1bfeec0e
Sharino <saruna.misiunas@gmail.com>
2014-12-09 09:02:04 +0200

    UI: component create/ edit button

--- UI/UI/scripts/views/dashboardListView.js
+++ UI/UI/scripts/views/dashboardListView.js
@@ -39,6 +39,7 @@
         render: function () {
             this.$el.html(this.template({ "Dashboards": this.collection.toJSON() }));
             $("body").removeClass("component-edit");
+//            $("body").addClass("bot-pic");
             return this;
         },
 
